72b1b8cff477be382f72f4a5c65a3e1769daf7df
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-03 12:59:28 +0530

    ADDED:Valid the last name

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -5,10 +5,20 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public boolean validFirstName(String firstName) {
+
         //PATTERN FOR VALID FIRST NAME
         String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternValidName);
         Matcher m = p.matcher(firstName);
+        return m.matches();
+    }
+
+    public boolean validLasttName(String lastName) {
+
+        //PATTERN FOR VALID LAST NAME
+        String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternValidName);
+        Matcher m = p.matcher(lastName);
         return m.matches();
     }
 }
