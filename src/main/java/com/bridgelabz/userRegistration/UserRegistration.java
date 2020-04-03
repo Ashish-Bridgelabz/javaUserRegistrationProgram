931c17944d74d3607e0b74748b22bb2ebbd859a6
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-03 11:20:54 +0530

    ADDED:Valid the first name

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.userRegistration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the user registration");
+    public boolean validFirstName(String firstName) {
+        //PATTERN FOR VALID FIRST NAME
+        String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternValidName);
+        Matcher m = p.matcher(firstName);
+        return m.matches();
     }
 }
