26bcfbc57bf4f511ffe3ba1f2b5138f1c9cb937e
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-03 15:03:36 +0530

    ADDED:Valided the indian mobile number

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -3,10 +3,11 @@ package com.bridgelabz.userRegistration;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class UserRegistration {
-    public boolean validFirstName(String firstName) {
+    //VARIABLE
+    private static final String COUNTRY_CODE = "91";
 
+    public boolean validFirstName(String firstName) {
         //PATTERN FOR VALID FIRST NAME
         String patternValidName = "^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternValidName);
@@ -28,6 +29,14 @@ public class UserRegistration {
         String patternValidEmailId = "^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$";
         Pattern p = Pattern.compile(patternValidEmailId);
         Matcher m = p.matcher(emialId);
+        return m.matches();
+    }
+
+    public boolean validMobileNumber(String mobileNumber) {
+        //PATTERN FOR VALID MOBILE NUMBER
+        String patternValidMobileNumber = "^"+COUNTRY_CODE+ "[ ][9876]{1}[0-9]{9}";
+        Pattern p = Pattern.compile(patternValidMobileNumber);
+        Matcher m = p.matcher(mobileNumber);
         return m.matches();
     }
 }
