d925627c0d155a45b8afc64024ddca49d57f082d
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-03 13:03:18 +0530

    ADDED:Test case for valid first name

--- src/test/java/com/bridgelabz/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userRegistration/UserRegistrationTest.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    //Test case for validate first name
+    //TEST CASE FOR VALIDATE FIRST NAME
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validFirstName("Ash");
+        Assert.assertEquals(true, result);
+    }
+    @Test
+    //TEST CASE FOR VALIDATE LAST NAME
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.validLasttName("Kum");
         Assert.assertEquals(true, result);
     }
 }
