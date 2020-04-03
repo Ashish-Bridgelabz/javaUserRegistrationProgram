4176d422c6a4d106b6050fa5b8f89904ef6eccd5
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-03 22:39:32 +0530

    ADDED:Finded atleast 1 special character

--- src/test/java/com/bridgelabz/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userRegistration/UserRegistrationTest.java
@@ -3,9 +3,9 @@ package com.bridgelabz.userRegistration;
 import org.junit.Assert;
 import org.junit.Test;
 
+//TEST CASE FOR VALIDATE FIRST NAME
 public class UserRegistrationTest {
     @Test
-    //TEST CASE FOR VALIDATE FIRST NAME
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validFirstName("Ash");
@@ -21,45 +21,57 @@ public class UserRegistrationTest {
     }
 
     @Test
+    //TEST CASE FOR VALIDATE EMAILID
     public void givenEmailId_WhenProper_ShouldReturnTrue() {
-        //TEST CASE FOR VALIDATE EMAILID
+
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validEmailId("asas49@gmail.com");
         Assert.assertEquals(true, result);
     }
 
     @Test
+    //TEST CASE FOR VALIDATE MOBILE NUMBER
     public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
-        //TEST CASE FOR VALIDATE MOBILE NUMBER
+
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validMobileNumber("91 9123423829");
         Assert.assertEquals(true, result);
     }
 
     @Test
+    //TEST CASE FOR MINIMUM 8 CHARACTERS
     public void givenLengthPassword_WhenProper_ShouldReturnTrue() {
 
-        //TEST CASE FOR MINIMUM 8 CHARACTERS
         UserRegistration validator = new UserRegistration();
         boolean result = validator.validLengthPassword("asdHdjsiw");
         Assert.assertEquals(true, result);
     }
 
     @Test
+    //TEST CASE FOR ATLIST 1 UPPER CASE
     public void givenUpperCase_WhenProper_ShouldReturnTrue() {
 
-        //TEST CASE FOR ATLIST 1 UPPER CASE
         UserRegistration validator = new UserRegistration();
         boolean result = validator.atLeastOneUpperCase("asHIshbridge");
         Assert.assertEquals(true, result);
     }
-    ////TEST CASE FOR ATLIST 1 NUMERICE VALUE
+
     @Test
+    //TEST CASE FOR ATLIST 1 NUMERICE VALUE
     public void givenNumericValue_WhenProper_ShouldReturnTrue() {
+
         UserRegistration validator = new UserRegistration();
         boolean result = validator.atleastOneNumericValue("Ashish123");
         Assert.assertEquals(true, result);
+    }
 
+    @Test
+    // TEST CASE FOR VALIDDATING PASSWORD FOR EXACTLY ONE SPECIAL CHARACTERT
+    public void givenSpecialCharacter_WhenProper_ShouldReturnTrue()
+    {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.exactlyOneSpecialCharacter("rahulRaj5@rahul");
+        Assert.assertEquals(true,result);
     }
 }
 
